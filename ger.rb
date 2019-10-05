d85a75d622b4ba2e04ddbf782dd46dacf225ca7c
Randel Jiao <randeljiao@gmail.com>
2019-10-05 06:23:16 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -2,10 +2,8 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(hash)
   value_arr = []
-  hash.collect do |key, value| 
-    value_arr << value
+  hash.collect do |key, value| if value
   end
-  value_arr.each_with_index {|num, index| value_arr[index + 1] <=> num }
 end
 
 puts key_for_min_value(hash)
