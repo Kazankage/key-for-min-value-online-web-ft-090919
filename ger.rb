15ebf913ff4bb51184dcc77d86c4af6b59453ce4
Randel Jiao <randeljiao@gmail.com>
2019-10-05 06:16:55 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -1,10 +1,9 @@
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(hash)
-  hash.collect do |key, value| 
-    key
+  hash.collect do |key, value| value <=> value
+    
   end
 end
 
 puts key_for_min_value(hash)
-puts hash[1]
